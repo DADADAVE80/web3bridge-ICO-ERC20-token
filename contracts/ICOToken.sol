281c7be11c8c9b290bce03cb819ac8d527403193
David Dada <daveproxy80@gmail.com>
2024-02-15 12:21:32 +0100

    add transfer function

--- contracts/ICOToken.sol
+++ contracts/ICOToken.sol
@@ -5,7 +5,7 @@ contract ICOToken{
     mapping(address account => uint256) private _balances;
     mapping(address account => mapping(address spender => uint256)) private _allowances;
 
-    uint256 private _totalSupply = 50 * 10 ** 18;
+    uint256 private _totalSupply = 50 * 10 ** uint(decimals()); // 18 decimals
 
     string private _name;
     string private _symbol;
@@ -33,5 +33,19 @@ contract ICOToken{
 
     function balanceOf(address account) public view returns (uint256){
         return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool){
+        require(msg.sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        require(amount > 0, "ERC20: transfer amount must be greater than zero");
+        uint256 senderBalance = _balances[msg.sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+
+        return true;
     }
 }
